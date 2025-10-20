91ff534da91599b13c6103b5080af47dcb8d6f96
0xrevault <revan@revault.one>
2025-10-20 21:17:16 +0800

    feat: add eas ci/cd build

--- app/services/background/index.ts
+++ app/services/background/index.ts
@@ -1,9 +1,9 @@
 // 作为 Service Worker 运行（ESM）
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("installed");
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//     console.log("installed");
+// });
   
-chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
-    if (msg?.type === "PING") sendResponse({ ok: true, ts: Date.now() });
-    return true;
-});
+// chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
+//     if (msg?.type === "PING") sendResponse({ ok: true, ts: Date.now() });
+//     return true;
+// });
