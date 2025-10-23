e04c0438fa46d1599b779d6d7d8f8e485c0f2891
0xrevault <revan@revault.one>
2025-10-23 13:04:52 +0800

    fix: EAS build version

--- app.plugin.js
+++ app.plugin.js
@@ -1,11 +1,11 @@
 const { withAppBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function withRenameApk(config) {
-    const rawName = (config.name || config.slug || 'app').toString();
-    const prefix = rawName.toLowerCase().replace(/[^0-9a-z._-]+/gi, '-');
+  const rawName = (config.name || config.slug || 'app').toString();
+  const prefix = rawName.toLowerCase().replace(/[^0-9a-z._-]+/gi, '-');
 
-    return withAppBuildGradle(config, (conf) => {
-        conf.modResults.contents += `
+  return withAppBuildGradle(config, (conf) => {
+    conf.modResults.contents += `
 
 // === rename APK: ${prefix}-{version}-{buildnumber}.apk ===
 android.applicationVariants.all { variant ->
@@ -13,12 +13,13 @@ android.applicationVariants.all { variant ->
     if (output.outputFileName != null && output.outputFileName.endsWith(".apk")) {
       def vName = variant.versionName ?: "0.0.0"
       def vCode = variant.versionCode
-      def safeV = vName.replaceAll(/[^0-9A-Za-z.\\-_]/, "-")
-      output.outputFileName = "${prefix}-\\${safeV}-\\${vCode}.apk"
+      def safeV = vName.replaceAll(/[^0-9A-Za-z._-]/, "-")
+
+      output.outputFileName = "${prefix}-" + safeV + "-" + vCode + ".apk"
     }
   }
 }
 `;
-        return conf;
-    });
+    return conf;
+  });
 };
